67c98e81b6e51b9c700b48cd7ae37ecb159c3aab
petes8796 <127255945+petes8796@users.noreply.github.com>
2025-11-12 23:22:44 +0530

    Update mongo.py

--- db/mongo.py
+++ db/mongo.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
-    raise Exception("MONGO_URI not found. Add it in your Render environment variables.")
+    raise Exception("MONGO_URI not found. Please set it in Render environment variables.")
 
 client = AsyncIOMotorClient(MONGO_URI)
-db = client["avrae"]
-applications = db.get_collection("applications")
+db = client["avrae"]  # your database name
+applications_collection = db.get_collection("applications")
