322dff984b480427fc9e4960b99c85b3d5a156a8
petes8796 <127255945+petes8796@users.noreply.github.com>
2025-11-12 23:17:14 +0530

    Update and rename mongodb.py to mongo.py

---
+++ db/mongo.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+if not MONGO_URI:
+    raise Exception("MONGO_URI not found. Add it in your Render environment variables.")
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["avrae"]
+applications = db.get_collection("applications")
--- db/mongodb.py
+++
@@ -1,9 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGO_URI = os.getenv("MONGO_URI")
-if not MONGO_URI:
-    raise RuntimeError("Missing MONGO_URI environment variable")
-
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["avrae"]
