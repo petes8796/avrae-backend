4267d4d7b829fe6c706f48dd421cb42619c76a67
petes8796 <127255945+petes8796@users.noreply.github.com>
2025-11-12 23:22:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from routes import applications
 
-app = FastAPI()
+app = FastAPI(title="Avrae Backend", version="1.0.0")
 
 # Register router
-app.include_router(applications.router, prefix="/applications", tags=["applications"])
+app.include_router(applications.router, prefix="/applications", tags=["Applications"])
 
 @app.get("/")
-def home():
+async def root():
     return {"message": "Avrae Backend is Live"}
